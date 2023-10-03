034c86b038f94dd5db1ef3b2b32d2d8c032413eb
windows <windows@microsoft.com>
2023-10-03 14:47:07 +0530

    change

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl,FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contactus',
@@ -12,7 +13,61 @@ c:string ="kumar1";
 fsize:number=100;
 
 kumar:string="ss";
+// mychan()
+// {
+// if(this.kumar==='abc')
+// {
+//   return "ppppppp"
+// }
+// }
+
+
+myselect:String = "";
+
+mydata:any = [
+  {
+    name:'kumar',
+    age:20,
+    phone:'87454845',
+    email:'kumar@gmail.com'
+  },
+  {
+    name:'singh',
+    age:25,
+    phone:'87454845',
+    email:'singh@gmail.com'
+  },
+  {
+    name:'mohit',
+    age:20,
+    phone:'87454845',
+    email:'mohit@gmail.com'
+  },
+  {
+    name:'kumar',
+    age:20,
+    phone:'87454845',
+    email:'kumar@gmail.com'
+  },
+  {
+    name:'kumar',
+    age:20,
+    phone:'87454845',
+    email:'kumar@gmail.com'
+  }
+];
+
+mybinddata = new FormGroup({
+  username:new FormControl()
+})
+
+myformsubmit()
+{
+  console.log(this.mybinddata.value);
+}
 
 }
+
+
 
 
