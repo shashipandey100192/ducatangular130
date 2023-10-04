a8dccd19693193b15008341098d39baa83b0d216
windows <windows@microsoft.com>
2023-10-04 14:03:15 +0530

    change new s

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingpageComponent } from './landingpage/landingpage.component';
-import { AboutComponent } from './about/about.component';
-import { ContactusComponent } from './contactus/contactus.component';
-import { MyerrorpageComponent } from './myerrorpage/myerrorpage.component';
-import { UserformComponent } from './userform/userform.component';
+
+
+
 
 const routes: Routes = [
-{
-  path:'',
-  component: LandingpageComponent
-},
-{
-  path:'about',
-  component:AboutComponent
-},
-{
-  path:'contact',
-  component:ContactusComponent
-},
-{
-  path:'myform',
-  component:UserformComponent
-},
-{
-  path:'**',
-  component:MyerrorpageComponent
-}
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { ContactusComponent } from './contactus/contactus.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
-import { LandingpageComponent } from './landingpage/landingpage.component';
 import { MynavbarComponent } from './mynavbar/mynavbar.component';
-import { MyerrorpageComponent } from './myerrorpage/myerrorpage.component';
 import { UserformComponent } from './userform/userform.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
@@ -17,11 +13,7 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
-    ContactusComponent,
-    LandingpageComponent,
     MynavbarComponent,
-    MyerrorpageComponent,
     UserformComponent
   ],
   imports: [
